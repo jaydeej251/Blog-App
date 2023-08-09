ecf7aeff480e1074b03501eb716f8af908a3b360
jaydeej251 <markdjjunio08@gmail.com>
2023-08-09 21:15:30 +0800

    fix: for deployment

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,24 @@ const multer  = require('multer')
 const uploadMiddleware = multer({ dest: 'uploads/' })
 const fs = require('fs');
 const Post = require('./models/Post')
+const dotenv = require('dotenv');
 
 
 const salt = bcrypt.genSaltSync(10);
-const secret ='askljhed12312asdf'
+
 const app = express()
+dotenv.config();
 
-
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({credentials:true, origin:process.env.corshost}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
 
 
-mongoose.connect('mongodb+srv://Blog-solo:ZUQ7k5YNI4wVlcjJ@cluster0.oilo4ej.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 
 app.post('/register', async (req,res) => {
@@ -49,7 +53,7 @@ app.post('/register', async (req,res) => {
       const passOk = bcrypt.compareSync(password, userDoc.password);
       if (passOk) {
         // logged in
-        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        jwt.sign({ username, id: userDoc._id }, process.env.secret, {}, (err, token) => {
           if (err) {
             console.error(err);
             return res.status(500).json({ error: 'Error creating JWT token' });
@@ -70,7 +74,7 @@ app.post('/register', async (req,res) => {
   
   app.get('/profile', (req,res) => {
     const {token} = req.cookies;
-    jwt.verify(token, secret, {}, (err,info) => {
+    jwt.verify(token,  process.env.secret, {}, (err,info) => {
       if (err) throw err;
       res.json(info);
     });
@@ -90,7 +94,7 @@ app.post('/register', async (req,res) => {
     fs.renameSync(path, newPath);
   
     const {token} = req.cookies;
-    jwt.verify(token, secret, {}, async (err,info) => {
+    jwt.verify(token,  process.env.secret, {}, async (err,info) => {
       if (err) throw err;
       const {title,summary,content} = req.body;
       const postDoc = await Post.create({
@@ -120,7 +124,7 @@ app.post('/register', async (req,res) => {
       }
   
       const { token } = req.cookies;
-      jwt.verify(token, secret, {}, async (err, info) => {
+      jwt.verify(token,  process.env.secret, {}, async (err, info) => {
         if (err) throw err;
         const { id, title, summary, content } = req.body;
         const postDoc = await Post.findById(id);
