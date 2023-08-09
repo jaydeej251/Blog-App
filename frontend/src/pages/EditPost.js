f0caf8049d192c6c00c2c6b1339624277f9c84d1
jaydeej251 <markdjjunio08@gmail.com>
2023-08-09 21:30:34 +0800

    fix. endpoints for deployment

--- frontend/src/pages/EditPost.js
+++ frontend/src/pages/EditPost.js
@@ -12,7 +12,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:4000/post/'+id)
+    fetch('https://blogit-sioi.onrender.com/post/'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
