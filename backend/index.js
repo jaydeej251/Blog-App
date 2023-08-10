05fbbdd16cbb450042df94c8eb67c4f1aeb1eec7
jaydeej251 <markdjjunio08@gmail.com>
2023-08-10 15:09:06 +0800

    fix cors error

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const salt = bcrypt.genSaltSync(10);
 const app = express()
 dotenv.config();
 
-app.use(cors({credentials:true, origin:process.env.corshost}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'))
